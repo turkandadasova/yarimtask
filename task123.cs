9228cdeb87232e56c62405bfd5b4656219d34878
Zubeydali <zubeydaalizada@gmail.com>
2024-10-18 16:45:47 +0400

    first commit

---
+++ task123.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Weapon weapon = new Weapon();
+            weapon.BulletCapasity = 20;
+            weapon.BulletNumber = 15;
+            weapon.GetRemainBulletCount();
+            weapon.Reload();
+            weapon.AddBullet();
+
+
+
+
+            bool f = false;
+            string operation;
+            do
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün  " +
+                    "1.Shoot metodu üçün" +
+                    " 2.GetRemainBulletCount metodu üçün" +
+                    "3.Reload metodu üçün" +
+                    "4.ChangeFireMode metodu üçün" +
+                    "5.Proqramdan dayandırmaq üçün");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "0";
+
+                }
+            }while (!f);
+
+
+        }
+    }
+}
